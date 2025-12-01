50762f1dcef87b4fe92e255c62eeea6cbd515fd2
pahae-tests <wahipahae@gmail.com>
2025-12-01 12:18:35 +0100

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} setNotification={setNotification} />
+  return <Component {...pageProps} />
+
 }
