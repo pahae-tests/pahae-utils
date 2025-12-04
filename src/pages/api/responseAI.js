2f38b585a31e166071f4f2ff3f07da6477a31a26
pahae-tests <wahipahae@gmail.com>
2025-12-04 13:50:40 +0100

    Update responseAI.js

--- src/pages/api/responseAI.js
+++ src/pages/api/responseAI.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
     try {
       const result = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDwVqL80ycP0sMykaPmeq_u7OdCJw35Otc`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_KEY}`,
         {
           method: "POST",
           headers: {
