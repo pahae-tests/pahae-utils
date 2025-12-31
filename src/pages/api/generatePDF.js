691e2e103425075e4daa6d148398a8d386f4c55c
pahae-tests <wahipahae@gmail.com>
2025-12-31 12:03:17 +0100

    Update generatePDF.js

--- src/pages/api/generatePDF.js
+++ src/pages/api/generatePDF.js
@@ -2,6 +2,10 @@ import chromium from "@sparticuz/chromium";
 import puppeteer from "puppeteer-core";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
